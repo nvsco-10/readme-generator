a981b9ecdaf44b1caa0e6e51163b09179584ba48
Neema Velasco <neemavelasco@gmail.com>
2022-03-23 15:27:39 -0500

    added inquirer prompt to test questions and collect data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -21,10 +22,17 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(response => {
+        console.log(response)
+    })
+}
 
 // Function call to initialize app
 init();
