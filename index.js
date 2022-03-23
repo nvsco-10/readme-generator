a8db9864fb3c9c33adaf59306b86b729baa47160
Neema Velasco <neemavelasco@gmail.com>
2022-03-23 15:15:47 -0500

    added starting questions in questions array

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ const inquirer = require("inquirer");
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    name: 'title',
+    type: 'input',
+    message: "What is the title of your project?"
+    },
+    {
+    name: 'description',
+    type: 'input',
+    message: "Provide a short description of your project."    
+    },
+    {
+    name: 'install',
+    type: 'input',
+    message: "What are the steps required to install your project?"    
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
